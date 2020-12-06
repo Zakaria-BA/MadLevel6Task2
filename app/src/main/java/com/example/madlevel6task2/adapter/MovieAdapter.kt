64b303528f1e00bf833e2476bb4f2c6841a81d6b
abdessz <zakaria.ben.abdessadak@hva.nl>
2020-12-06 14:43:01 +0100

    .

--- app/src/main/java/com/example/madlevel6task2/adapter/MovieAdapter.kt
+++ app/src/main/java/com/example/madlevel6task2/adapter/MovieAdapter.kt
@@ -1,34 +1,43 @@
 package com.example.madlevel6task2.adapter
 
+import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.example.madlevel6task2.Model.Movie
 import com.example.madlevel6task2.R
+import kotlinx.android.synthetic.main.movie_overview_item.view.*
 
 class MovieAdapter(private val movies: List<Movie>, private val onClick: (Movie) -> Unit) : RecyclerView.Adapter<MovieAdapter.ViewHolder>(){
-    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView){
-        init {
+    private lateinit var context: Context
 
-        }
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        context = parent.context
 
-        fun databind(movie: Movie){
-
-        }
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MovieAdapter.ViewHolder {
         return ViewHolder(
-            LayoutInflater.from(parent.context).inflate(R.layout.movie_overview_item, parent, false)
-        )
-    }
+            LayoutInflater.from(context).inflate(R.layout.movie_overview_item, parent, false)
 
-    override fun onBindViewHolder(holder: MovieAdapter.ViewHolder, position: Int) {
-        holder.databind(movies[position])
+        )
     }
 
     override fun getItemCount(): Int {
         return movies.size
+    }
+
+    inner class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        init {
+            itemView.setOnClickListener {onClick(movies[adapterPosition]) }
+        }
+
+        fun bind(movie: Movie) {
+            itemView.tvMovieName.text = movie.title
+            Glide.with(context).load(movie.getPosterPath()).into(itemView.ivPoster)
+        }
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(movies[position])
     }
 }
